ca8e004ac80ae33447e75a12c81baa27c1806de2
Xin Ma <xma24@buffalo.edu>
2021-06-17 14:17:16 -0500

    Update mnist_sample.py

--- mnist_sample.py
+++ mnist_sample.py
@@ -1,18 +1,18 @@
-import torch
-from torch import nn
-import pytorch_lightning as pl
-from torch.nn import functional as F
-
-from torch.utils.data import DataLoader, random_split, Dataset
-from torchvision.datasets import MNIST
+from pytorch_lightning.loggers import TensorBoardLogger
+import argparse
 import os
-from torchvision import datasets, transforms
-
-import numpy as np
 import pickle
-
 import subprocess
 import zipfile
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import datasets, transforms
+from torchvision.datasets import MNIST
 
 
 class CustomizeDataset(Dataset):
@@ -36,14 +36,10 @@ class CustomizeDataset(Dataset):
         return data_ret, labels_ret
 
 
-import argparse
-import os
-
-
 def parameter_setting(cuda_index):
     parser = argparse.ArgumentParser()
 
-    ## dynamic
+    # dynamic
     parser.set_defaults(gpus='0', max_epochs=2000)
     parser.add_argument('--batch_size', default=64)
     parser.add_argument('--no_cuda', default=False)
@@ -51,8 +47,8 @@ def parameter_setting(cuda_index):
     parser.add_argument('--epoch_number', type=int, default=1000)
     parser.add_argument('--lr', default=0.001)
 
-    ## fixed
-    parser.add_argument('--data_root', default="/data/xma24/", type=str)
+    # fixed
+    parser.add_argument('--data_root', default="./datasets/", type=str)
     parser.add_argument('--pytorch_data_path', default="./pytorch_data/")
     parser.add_argument('--result_folder', default="./output/", type=str)
     parser.add_argument('--download_require', default=True)
@@ -65,11 +61,11 @@ def parameter_setting(cuda_index):
     os.makedirs(args.data_root, exist_ok=True)
     os.makedirs(args.pytorch_data_path, exist_ok=True)
 
-
     args.use_cuda = not args.no_cuda and torch.cuda.is_available()
     torch.manual_seed(args.seed)
 
-    args.kwargs = {'num_workers': 8, 'pin_memory': True} if args.use_cuda else {}
+    args.kwargs = {'num_workers': 8,
+                   'pin_memory': True} if args.use_cuda else {}
 
     return args
 
@@ -120,26 +116,32 @@ class LightningMNISTClassifier(pl.LightningModule):
         # transformations = transforms.Compose([transforms.Scale(32),transforms.ToTensor()])
 
         if not self.args.drive_download_require:
-            mnist_train_data = MNIST(os.getcwd(), train=True, download=True, transform=transform)
+            mnist_train_data = MNIST(
+                os.getcwd(), train=True, download=True, transform=transform)
 
             train_data_dict = {}
             train_data = mnist_train_data.data
             train_labels = mnist_train_data.targets
 
-            train_data_list = [train_data[i].numpy() for i in range(train_data.shape[0])]
-            train_labels_list = [train_labels[i].numpy() for i in range(train_data.shape[0])]
+            train_data_list = [train_data[i].numpy()
+                               for i in range(train_data.shape[0])]
+            train_labels_list = [train_labels[i].numpy()
+                                 for i in range(train_data.shape[0])]
 
             train_data_dict["data"] = train_data_list
             train_data_dict["labels"] = train_labels_list
 
-            mnist_test_data = MNIST(os.getcwd(), train=False, download=True, transform=transform)
+            mnist_test_data = MNIST(
+                os.getcwd(), train=False, download=True, transform=transform)
 
             test_data_dict = {}
             test_data = mnist_test_data.data
             test_labels = mnist_test_data.targets
 
-            test_data_list = [test_data[i].numpy() for i in range(test_data.shape[0])]
-            test_labels_list = [test_labels[i].numpy() for i in range(test_data.shape[0])]
+            test_data_list = [test_data[i].numpy()
+                              for i in range(test_data.shape[0])]
+            test_labels_list = [test_labels[i].numpy()
+                                for i in range(test_data.shape[0])]
 
             test_data_dict["data"] = test_data_list
             test_data_dict["labels"] = test_labels_list
@@ -157,25 +159,27 @@ class LightningMNISTClassifier(pl.LightningModule):
             test_dataset = mnist_test_data
 
         else:
-            file_name = os.path.join(args.data_root, args.dataset_name + ".zip")
+            file_name = os.path.join(
+                args.data_root, args.dataset_name + ".zip")
             file_folder = os.path.join(args.data_root, args.dataset_name)
 
-            ## ****************************************************Downloading data ***************************************
-            ## file already exists
+            # ****************************************************Downloading data ***************************************
+            # file already exists
             if os.path.exists(file_name):
-                ## folder already exists
+                # folder already exists
                 if os.path.exists(file_folder):
                     print("Data is ready")
-                ## folder not exists; extract it
+                # folder not exists; extract it
                 else:
                     os.makedirs(file_folder, exist_ok=True)
                     with zipfile.ZipFile(file_name, "r") as zip_ref:
                         zip_ref.extractall(file_folder)
                     print("{}".format("Finish Extracting the Dataset ... "))
-            ## file not exists; download it
+            # file not exists; download it
             else:
                 print("{}".format("Downloading the Dataset ... "))
-                subprocess.call(["sh", "./datasets_download_" + args.dataset_name + ".sh"])
+                subprocess.call(
+                    ["sh", "./datasets_download_" + args.dataset_name + ".sh"])
                 print("{}".format("Finish Downloading the Dataset ... "))
                 os.makedirs(file_folder, exist_ok=True)
                 with zipfile.ZipFile(file_name, 'r') as zip_ref:
@@ -198,15 +202,21 @@ class LightningMNISTClassifier(pl.LightningModule):
             test_dict["data"] = np.array(test_data)
             test_dict["labels"] = np.array(test_labels)
 
-            train_dataset = CustomizeDataset(train_dict, transform=args.transform)
+            train_dataset = CustomizeDataset(
+                train_dict, transform=args.transform)
             # print("{}".format(train_dataset))
-            test_dataset = CustomizeDataset(test_dict, transform=args.transform)
+            test_dataset = CustomizeDataset(
+                test_dict, transform=args.transform)
 
-            train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
-            test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
+            train_dataloader = DataLoader(
+                train_dataset, batch_size=args.batch_size)
+            test_dataloader = DataLoader(
+                test_dataset, batch_size=args.batch_size)
 
-        self.mnist_train_loader = DataLoader(train_dataset, batch_size=self.args.batch_size, shuffle=True)
-        self.mnist_test_loader = DataLoader(test_dataset, batch_size=self.args.batch_size)
+        self.mnist_train_loader = DataLoader(
+            train_dataset, batch_size=self.args.batch_size, shuffle=True)
+        self.mnist_test_loader = DataLoader(
+            test_dataset, batch_size=self.args.batch_size)
 
     def train_dataloader(self):
         return self.mnist_train_loader
@@ -238,16 +248,20 @@ class LightningMNISTClassifier(pl.LightningModule):
         return {'loss': loss, "train_prediction": train_prediction, "train_y": y, 'log': logs}
 
     def training_epoch_end(self, outputs):
-        epoch_train_loss = torch.stack([x["log"]['train_loss'] for x in outputs]).mean()
+        epoch_train_loss = torch.stack(
+            [x["log"]['train_loss'] for x in outputs]).mean()
         print("\n" + "*" * 10 + "epoch_train_loss: {} ".format(epoch_train_loss))
 
-        epoch_train_pred = torch.cat([x['train_prediction'] for x in outputs], dim=0)
+        epoch_train_pred = torch.cat(
+            [x['train_prediction'] for x in outputs], dim=0)
         epoch_train_y = torch.cat([x['train_y'] for x in outputs], dim=0)
 
-        epoch_train_acc = epoch_train_pred.eq(epoch_train_y).view(-1).float().mean()
+        epoch_train_acc = epoch_train_pred.eq(
+            epoch_train_y).view(-1).float().mean()
         print("*" * 10 + "epoch_train_acc : {}\n ".format(epoch_train_acc))
 
-        tensorboard_logs = {'epoch_train_loss': epoch_train_loss, "epoch_train_acc": epoch_train_acc}
+        tensorboard_logs = {
+            'epoch_train_loss': epoch_train_loss, "epoch_train_acc": epoch_train_acc}
         return {'epoch_train_loss': epoch_train_loss, 'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_idx):
@@ -261,12 +275,17 @@ class LightningMNISTClassifier(pl.LightningModule):
         return {'validation_loss': validation_loss, "validation_prediction": validation_prediction, "validation_y": y}
 
     def validation_epoch_end(self, outputs):
-        epoch_validation_loss = torch.stack([x['validation_loss'] for x in outputs]).mean()
-        print("\n" + "*" * 10 + "epoch_validation_loss: {} ".format(epoch_validation_loss))
+        epoch_validation_loss = torch.stack(
+            [x['validation_loss'] for x in outputs]).mean()
+        print("\n" + "*" * 10 +
+              "epoch_validation_loss: {} ".format(epoch_validation_loss))
 
-        epoch_validation_pred = torch.cat([x['validation_prediction'] for x in outputs], dim=0)
-        epoch_validation_y = torch.cat([x['validation_y'] for x in outputs], dim=0)
-        epoch_validation_acc = epoch_validation_pred.eq(epoch_validation_y).view(-1).float().mean()
+        epoch_validation_pred = torch.cat(
+            [x['validation_prediction'] for x in outputs], dim=0)
+        epoch_validation_y = torch.cat(
+            [x['validation_y'] for x in outputs], dim=0)
+        epoch_validation_acc = epoch_validation_pred.eq(
+            epoch_validation_y).view(-1).float().mean()
 
         print("*" * 10 + "epoch_validation_acc : {}\n ".format(epoch_validation_acc))
 
@@ -310,7 +329,6 @@ args.expr_index = 1
 args.layer_number = 3
 args.dataset_name = "MNIST"
 
-from pytorch_lightning.loggers import TensorBoardLogger
 
 logger_name = "-".join(
     ["p-", args.project_name, "e-", str(args.expr_index), "l_n-", str(args.layer_number), "d_n-", args.dataset_name])
@@ -321,7 +339,7 @@ trainer = pl.Trainer(gpus=cuda_index, max_epochs=args.epoch_number)
 
 trainer.fit(model)
 
-### ************************ Changes ************************
-## custormize the tensorboard logger
-## add "tensorboard settings"
-## using google drive to download data automatically
+# ************************ Changes ************************
+# custormize the tensorboard logger
+# add "tensorboard settings"
+# using google drive to download data automatically
